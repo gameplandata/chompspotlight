70cb28b47e9bf36f92326977e36099887dbc2647
Kevin-Daniel <kevinadaniel7@gmail.com>
2024-02-28 14:50:41 -0500

    add header and footer to feed

--- client/src/Pages/Feed.js
+++ client/src/Pages/Feed.js
@@ -1,14 +1,20 @@
 import React, { useState, useEffect } from 'react';
 import Post from '../Components/Post';
+import Header from '../Components/HeaderWithoutSearch';
+import Footer from '../Components/Footer';
 
 function Feed() {
 
     return (
-        <div className="bg-white min-h-screen p-4">
-           <Post />
-           <Post />
-           <Post />
-           <Post />
+        <div className="relative bg-white min-h-screen pt-20 pb-[10vh]">
+            <Header />
+            <div className="my-5">
+                <Post />
+                <Post />
+                <Post />
+                <Post />
+            </div>
+            <Footer />
         </div>
     );
 }
