2ad25e4fcdd3c257258af6f304116428b0d27e3a
Kevin-Daniel <kevinadaniel7@gmail.com>
2024-02-29 23:59:19 -0500

    mysql setup and example

--- server/database.js
+++ server/database.js
@@ -1,11 +1,12 @@
 const mysql = require('mysql2/promise');
+require('dotenv').config()
 
 // Database configuration
 const dbConfig = {
-    host: 'localhost',
-    user: 'root', 
-    password: 'enter password here', 
-    database: 'enter database name here' 
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER, 
+    password: process.env.DB_PASSWORD, 
+    database: process.env.DB_DATABASE 
   };
 
 // Creates connection pool 
